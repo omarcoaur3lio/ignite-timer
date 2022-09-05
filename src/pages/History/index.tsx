6193caaf2cb9a9b837a02565a1e0ed56defeec2b
omarcoaur3lio <sgtmarcoaurelio@icloud.com>
2022-09-05 16:57:20 -0300

    :rocket: Second module finished

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,62 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
